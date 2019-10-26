6fbeda369c311ded609d04754f215e798a91645f
JonathanQuang <jquang1000@gmail.com>
2019-10-26 00:49:33 -0400

    dump user profile when sending get request to index

--- piazzaApi.py
+++ piazzaApi.py
@@ -1,3 +1,21 @@
 from piazza_api import Piazza
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
 p = Piazza()
 p.user_login("jquang1000@gmail.com", "hackgtdummy")
+#print(p.get_user_profile())
+
+class HelloWorld(Resource):
+    def get(self):
+        return p.get_user_profile()
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
