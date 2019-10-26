748785145237d23de5fd5676b32fac74151ee3d5
Dhruva Bansal <dbansal36@gatech.edu>
2019-10-26 04:01:02 -0400

    MongoDB for Rooms

--- piazzaApi.py
+++ piazzaApi.py
@@ -5,10 +5,14 @@ from flask_restful import Resource, Api
 from flask import jsonify
 from webargs import fields, validate
 from webargs.flaskparser import use_kwargs, parser
+from flask_pymongo import PyMongo
 
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
 api = Api(app)
+mongo = PyMongo(app)
+
 
 get = Piazza()
 get.user_login("jquang1000@gmail.com", "hackgtdummy")
@@ -52,8 +56,41 @@ class Search(Resource):
 	def get(self, query):
 		return jsonify(cs101.search_feed(query))
 
+class EnterRoom(Resource):
+	args = {
+		'id': fields.Str(
+			required=True,
+		),
+		'room': fields.Str(
+			required=True,
+		),
+	}
+	@use_kwargs(args)
+	def post(self, id, room):
+		if mongo.db.room.find_one({"id" : id}) is None:
+			mongo.db.room.insert_one({"id" : id})
+		return mongo.db.room.find_one({"id" : id}) != None
+
+class ExitRoom(Resource):
+	args = {
+		'id': fields.Str(
+			required=True,
+		),
+		'room': fields.Str(
+			required=True,
+		),
+	}
+	@use_kwargs(args)
+	def post(self, id, room):
+		mongo.db.room.delete_one({"id" : id})
+		return mongo.db.room.find_one({"id" : id}) is None
+
+
+
 api.add_resource(Post, '/post/')
 api.add_resource(Search, '/search/')
+api.add_resource(EnterRoom, '/enterRoom/')
+api.add_resource(ExitRoom, '/exitRoom/')
 
 
 if __name__ == '__main__':
