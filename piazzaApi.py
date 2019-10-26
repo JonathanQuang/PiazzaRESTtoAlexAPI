cafa0e047b4d3e7b798b76fd1fb110bdf1d16af8
Dhruva Bansal <dbansal36@gatech.edu>
2019-10-26 01:21:33 -0400

    Made Post Endpoint

--- piazzaApi.py
+++ piazzaApi.py
@@ -8,23 +8,22 @@ from flask import jsonify
 app = Flask(__name__)
 api = Api(app)
 
-post = PiazzaRPC("k26wh1bxb6imp")
-post.user_login("jquang1000@gmail.com", "hackgtdummy")
-
-
-#p = Piazza()
-#p.user_login("jquang1000@gmail.com", "hackgtdummy")
-#network = p.network("k26wh1bxb6imp")
-
-#line below is an example of how to post a question
-#network.create_post("question",['polls'], "CS 101", "I wish I was a living meme")
-
 get = Piazza()
 get.user_login("jquang1000@gmail.com", "hackgtdummy")
 cs101 = get.network("k26wh1bxb6imp")
 
 class HelloWorld(Resource):
 	def get(self):
+		allPosts = {}
+		posts = cs101.iter_all_posts(limit=10)
+		index = 0
+		for p in posts:
+		    allPosts[index] = p
+		    index = index + 1
+		return jsonify(allPosts)
+
+	def post(self):
+		cs101.create_post("question",['polls'], "CS 101", "no jobs")
 		allPosts = {}
 		posts = cs101.iter_all_posts(limit=10)
 		index = 0
