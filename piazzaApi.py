0c8f0b9a86181c12f5e9682a2ca1cacafdc27568
Dhruva Bansal <dbansal36@gatech.edu>
2019-10-26 00:51:30 -0400

    Add post get

--- piazzaApi.py
+++ piazzaApi.py
@@ -1,3 +1,9 @@
 from piazza_api import Piazza
 p = Piazza()
-p.user_login("jquang1000@gmail.com", "hackgtdummy")
+p.user_login("dhruva.bansal00@gmail.com", "DhrBan002k")
+cs101 = p.network("k26wh1bxb6imp")
+posts = cs101.iter_all_posts(limit=10)
+for post in posts:
+	print(post)
+all_users = cs101.get_all_users()
+print(all_users)
